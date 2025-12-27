80a0017857f70e07f02bafc293c99c88fe471237
dvtzoe <ohmtzoe@gmail.com>
2025-12-27 20:43:05 +0700

    feat: use Docker
    add backend/Dockerfile and docker-compose.yml
    move config.json to backend/
    add Makefile
    changes python version from >=3.13 to >=3.12
    able to use custom config file from CONFIG_PATH env var for backend

--- backend/src/config_loader.py
+++ backend/src/config_loader.py
@@ -1,6 +1,10 @@
 import json
+import os
+
+from dotenv import load_dotenv
 
 from schemas import Config
 
-with open(r"../config.json", "r") as config_file:
-    config = Config(**json.load(config_file)["backend"])  # pyright: ignore[reportAny]
+load_dotenv()
+with open(os.getenv("CONFIG_PATH") or r"../config.json", "r") as config_file:
+    config = Config(**json.load(config_file))  # pyright: ignore[reportAny]
